a9f2d00ff69b8472f69f7a3ad300d8f7e3981552
ThenuSathees <thenujasatheeskumar@gmail.com>
2024-10-17 11:48:44 +0530

    merging with cane_rental

--- Carrental/obj/Debug/net8.0/Carrental.AssemblyInfo.cs
+++ Carrental/obj/Debug/net8.0/Carrental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Carrental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ce5731cc3fb73a1a73632b3513d361cc889d76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07fb5c0da9a0be64a0215a8ca6564eb711cdbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Carrental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Carrental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
